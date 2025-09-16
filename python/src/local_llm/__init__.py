c155970e5a726e9f9c7662fceb2c2a464d9539ff
Julian Bernado <jubernado@DN0a2289bb.SUNet>
2025-09-16 16:06:23 -0700

    chore: bump version to 0.1.1 (Python & R)

---
+++ python/src/local_llm/__init__.py
@@ -0,0 +1,24 @@
+"""local_llm: minimal client + programmatic server launcher.
+
+Public API:
+	get_response(prompt, model="local", ...)
+	grammar(path)
+	multiple_choice_grammar(choices, save_dir, name, thinking=True)
+	start_server(...)
+
+The start_server function launches a llama-server process and returns a ServerHandle
+object that can be used to manage its lifecycle. Intended for local research use.
+"""
+
+from .client import get_response, grammar, multiple_choice_grammar
+from .server import start_server, ServerHandle
+
+__all__ = [
+	"get_response",
+	"grammar",
+	"multiple_choice_grammar",
+	"start_server",
+	"ServerHandle",
+]
+
+__version__ = "0.1.1"
